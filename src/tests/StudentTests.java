1e1ef5223660d11b5d89707dd6dee0981fdafeaf
JrWang0930 <736165+JrWang0930@users.noreply.github.com>
2020-11-22 00:42:47 -0500

    init pro 1

---
+++ src/tests/StudentTests.java
@@ -0,0 +1,22 @@
+package tests;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import blackjack.CardValue;
+
+
+/**
+ * Please put your own test cases into this file, so they can be tested
+ * on the server.
+*/
+
+public class StudentTests {
+	@Test
+	public void test1() {
+		System.out.print(CardValue.values().length);
+		
+		
+		
+	}
+	
+}
